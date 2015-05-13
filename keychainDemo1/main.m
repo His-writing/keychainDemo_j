fa4b70084fef52bc6ca5d6909db523ad0ded75a4
LOVE_IOS <shenzhenguo110@163.com>
2015-05-13 16:21:54 +0800

    keychainDemo
    keychainDemo

---
+++ keychainDemo1/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  keychainDemo1
+//
+//  Created by DT on 14-9-9.
+//  Copyright (c) 2014年 DT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
